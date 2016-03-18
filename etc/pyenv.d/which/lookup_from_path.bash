a4b5aa5906497a08b0cfb781286d94b23d5ec3fd
Daniel Hahler <git@thequod.de>
2016-03-19 00:04:45 +0100

    Merge remove_from_path from pyenv

--- etc/pyenv.d/which/lookup_from_path.bash
+++ etc/pyenv.d/which/lookup_from_path.bash
@@ -1,34 +1,13 @@
-expand_path() {
-  if [ ! -d "$1" ]; then
-    return 1
-  fi
-
-  local cwd="$(pwd)"
-  cd "$1"
-  pwd
-  cd "$cwd"
-}
-
 remove_from_path() {
-  local path_to_remove="$(expand_path "$1")"
-  local result=""
-
-  if [ -z "$path_to_remove" ]; then
-    echo "${PATH}"
-    return
-  fi
-
-  local paths
-  IFS=: paths=($PATH)
-
-  for path in "${paths[@]}"; do
-    path="$(expand_path "$path" || true)"
-    if [ -n "$path" ] && [ "$path" != "$path_to_remove" ]; then
-      result="${result}${path}:"
-    fi
+  local path_to_remove="$1"
+  local path_before
+  local result=":$PATH:"
+  while [ "$path_before" != "$result" ]; do
+    path_before="$result"
+    result="${result//:$path_to_remove:/:}"
   done
-
-  echo "${result%:}"
+  result="${result%:}"
+  echo "${result#:}"
 }
 
 lookup_from_path() {
